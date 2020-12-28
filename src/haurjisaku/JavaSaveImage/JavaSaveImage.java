6b4c07f4988c54c0a01cade88d443ecb3c1c2a4e
harujisaku <harujisaku@gmail.com>
2020-12-29 05:07:25 +0900

    add cmd option

--- src/haurjisaku/JavaSaveImage/JavaSaveImage.java
+++ src/haurjisaku/JavaSaveImage/JavaSaveImage.java
@@ -19,40 +19,48 @@ public class JavaSaveImage{
 	static String url = "https://www.so-net.ne.jp/search/image/";
 	static Pattern p = Pattern.compile("<a.*?href\\s*=\\s*[\"|'](https?://.*?)[\"|'].*? rel=\"search_result\".*?>");
 	static Matcher m;
-	static String html="";
-	public JavaSaveImage(){
-		
-	}
-	
+	static String html="",option="",texts="raspberry pi",path="";
+	static int length=5;
+	static String helpMessage = "-h,-help : help , this message ヘルプ　このメッセージ\r\n-l,-len,-length : length　検索ページの長さ\r\n-o,-option : option　検索エンジンに指定するオプション\r\n\tlanguage,ysp_q,size,end,imtype,format,ss_view,from,q_type,view,adult,start\r\n-t,-text : search text　検索するテキスト";
+
 	public static void main(String[] args) {
-		if (args.length==0) {
-			html=getHTML("とある科学の超電磁砲");
-		}else if (args.length!=1) {
-			return;
-		}else {
-			html=getHTML(args[0]);
+		for (String str :args ) {
+			System.out.println(str);
 		}
-		save();
 		int a = 0;
-		while(true){
-			System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-			a+=20;
-			System.out.println(a);
-			if (args.length==0) {
-				html=getHTML(20,"raspberry pi",a);
-			}else if (args.length!=1) {
+		if (args.length==0) {
+			System.out.println(helpMessage);
+			return;
+		}
+		for (int i=0,len=args.length;i<len ;i++ ) {
+			if ("-h".equals(args[i])||"-help".equals(args[i])) {
+				System.out.println(helpMessage);
 				return;
-			}else {
-				html=getHTML(20,args[0],a);
+			}else if ("-len".equals(args[i])||"-l".equals(args[i])||"-length".equals(args[i])) {
+				length=Integer.parseInt(args[++i]);
+			}else if("-o".equals(args[i])||"-option".equals(args[i])){
+				option=args[++i];
+			}else if ("-t".equals(args[i])||"-text".equals(args[i])) {
+				texts=args[++i];
+			}else if ("-p".equals(args[i])||"-path".equals(args[i])) {
+				path=args[++i];
+			}else{
+				System.out.println("引数指定の誤り：未知の引数が指定されました");
+				System.out.println(helpMessage);
+				return;
 			}
+		}
+		System.out.println(texts);
+		while(length>a){
+			html=getHTML(20,texts,a*20);
 			save();
+			a++;
 		}
 	}
 	
 	private static void save(){
-		System.out.println(html);
 		m=p.matcher(html);
-		File file = new File("1.jpg");
+		File file = new File(path+"1.jpg");
 		int i=0;
 		while(m.find()){
 			System.out.println(getURL());
@@ -63,7 +71,7 @@ public class JavaSaveImage{
 			try {
 				while(file.exists()){
 					i++;
-					file = new File(i+".jpg");
+					file = new File(path+i+".jpg");
 				}
 				FileOutputStream fo = new FileOutputStream(file);
 				BufferedOutputStream bw = new BufferedOutputStream(fo);
@@ -74,52 +82,25 @@ public class JavaSaveImage{
 		}
 	}
 	
-	private static String getHTML(String text){
-		try {
-			
-		String sendUrl;
-		try {
-			sendUrl = url+"?query="+URLEncoder.encode(text,"UTF-8");
-		} catch(UnsupportedEncodingException e) {
-			sendUrl = url+text;
-		}
-		HttpURLConnection connection = (HttpURLConnection) new URL(sendUrl).openConnection();
-		int responseCode = connection.getResponseCode();
-		InputStream inputStream;
-		if (200 <= responseCode && responseCode <= 299) {
-			inputStream = connection.getInputStream();
-		}else{
-			inputStream = connection.getErrorStream();
-		}
-		BufferedReader in = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
-		StringBuilder response = new StringBuilder();
-		String currentLine;
-		while((currentLine = in.readLine())!=null){
-			response.append(currentLine);
-		}
-		in.close();
-		return response.toString();
-		} catch(Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
 	private static String getHTML(int count,String text,int start){
 		try {
 			
 		String sendUrl;
 		try {
-			sendUrl = url+"?count="+String.valueOf(count)+"&query="+URLEncoder.encode(text,"UTF-8")+"&start="+String.valueOf(start);
+			sendUrl = url+"?count="+String.valueOf(count)+"&query="+URLEncoder.encode(text,"UTF-8")+option+"&start="+String.valueOf(start);
 		} catch(UnsupportedEncodingException e) {
-			sendUrl = url+"?count="+String.valueOf(count)+"&query="+text+"&start="+String.valueOf(start);
+			sendUrl = url+"?count="+String.valueOf(count)+"&query="+text+option+"&start="+String.valueOf(start);
 		}
+		System.out.println(sendUrl);
 		HttpURLConnection connection = (HttpURLConnection) new URL(sendUrl).openConnection();
+		urlcon.setRequestParameter("User-Agent" , "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0");
 		int responseCode = connection.getResponseCode();
 		InputStream inputStream;
 		if (200 <= responseCode && responseCode <= 299) {
 			inputStream = connection.getInputStream();
 		}else{
+			System.out.println("error");
+			System.out.println(responseCode);
 			inputStream = connection.getErrorStream();
 		}
 		BufferedReader in = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
@@ -214,5 +195,31 @@ public class JavaSaveImage{
 			return out;
 		}
 		return bi;
+	}
+	
+	private static void getUserAgent(){
+		try {
+			String sendUrl ="https://testpage.jp/tool/ip_user_agent.php";
+			HttpURLConnection connection = (HttpURLConnection) new URL(sendUrl).openConnection();
+			int responseCode = connection.getResponseCode();
+			InputStream inputStream;
+			if (200 <= responseCode && responseCode <= 299) {
+				inputStream = connection.getInputStream();
+			}else{
+				System.out.println("error");
+				inputStream = connection.getErrorStream();
+			}
+			BufferedReader in = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
+			StringBuilder response = new StringBuilder();
+			String currentLine;
+			while((currentLine = in.readLine())!=null){
+				response.append(currentLine);
+			}
+			in.close();
+			System.out.println(response.toString());
+			
+		} catch(Exception e) {
+			
+		}
 	}
 }
