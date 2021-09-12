0bd5b37ec5b83f5e2504edde3d3fbec83fa3a93a
haru <64310155+usbharu@users.noreply.github.com>
2021-09-13 08:20:16 +0900

    不要なメッセージを削除

--- src/harujisaku/javasaveimage/net/ImageConnection.java
+++ src/harujisaku/javasaveimage/net/ImageConnection.java
@@ -1,13 +1,11 @@
 package harujisaku.javasaveimage.net;
 
 import harujisaku.javasaveimage.imageio.NetImage;
-import harujisaku.javasaveimage.util.Message;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.SocketTimeoutException;
 import java.net.URL;
 
 public class ImageConnection {
@@ -16,19 +14,12 @@ public class ImageConnection {
 		urlcon.setConnectTimeout(timeOut);
 		urlcon.setReadTimeout(timeOut);
 		BufferedImage bi;
-		try {
 			int responseCode=urlcon.getResponseCode();
 			if (200<=responseCode&&responseCode<=299) {
 				bi = ImageIO.read(urlcon.getInputStream());
 			}else{
-				System.out.println(Message.ERROR);
-				System.out.println(responseCode);
-				bi=null;
+				return null;
 			}
-		} catch(SocketTimeoutException e) {
-			System.out.println(Message.TIME_OUT);
-			return null;
-		}
 		NetImage netImage = new NetImage(bi,imageURL,urlcon.getURL().getFile());
 		return netImage;
 	}
